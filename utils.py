3e92feb7b868b8395cecbf4e1ac32a5d796e8503
root <maxibove13@gmail.com>
2022-02-26 20:44:32 +0000

    Improved code to test pretrained generator

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import os
 import config
 from PIL import Image
 from torchvision.utils import save_image
+import matplotlib.pyplot as plt
 
 def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
     """
@@ -11,7 +12,6 @@ def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
     A net's weights and biases are contain in the model's parameter. A state_dict is simply a dict object that maps each layer to its parameter tensor.
     Here we save the net and optimizer state_dict for later use.
     """
-    print("=> Saving checkpoint")
     checkpoint = {
         "state_dict": model.state_dict(),
         "optimizer": optimizer.state_dict()
@@ -32,22 +32,35 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
         param_group["lr"] = lr
 
 
-def plot_examples(low_res_folder, G, id):
+def plot_examples(low_res_folder, G, idx):
     """
-    Function to plot some generated high_res images examples
+    Function to plot some generated super_res images examples
     """
-    files = os.listdir(low_res_folder)
+    files = [f for f in os.listdir('datasets/testing/') if '.png' in f]
 
     # Put Generator in evaluation mode
     G.eval()
 
     for file in files:
         image = Image.open(low_res_folder + file)
+        im_arr = np.asarray(image)[:,:,0:3] # Only RGB channels.
         with torch.no_grad():
             upscaled_img = G(
-                config.test_transform(image=np.asarray(image))["image"].unsqueeze(0).to(config.DEVICE)
+                config.test_transform(image=im_arr)["image"].unsqueeze(0).to(config.DEVICE)
             )
-        save_image(upscaled_img * 0.5 + 0.5, f"saved/{file}_{id}")
+        save_image(upscaled_img * 0.5 + 0.5, f"datasets/testing/sr/{file[:-4]}_sr.png")
+
     
     # Put generator in training mode
     G.train()
+
+def plot_loss(epoch, loss_disc, loss_gen):
+  x = np.arange(0, epoch)
+  plt.plot(x, loss_disc, label='Discriminator loss')
+  plt.plot(x, loss_gen, label='Generator loss')
+
+  plt.title('Evolution of losses through epochs')
+  plt.xlabel('epochs')
+  plt.ylabel('loss')
+  plt.legend(loc='upper right')
+  plt.grid()
